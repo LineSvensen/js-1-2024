72936a3554cc393033935d142c69f4477422a29a
MSI\lines <line.sven@live.no>
2024-01-11 12:35:20 +0100

    saving before adding a plugin

---
+++ functions/js/script.js
@@ -0,0 +1,11 @@
+// i want to create a function in my calculator app
+// to sum three numbers
+// please write this function for me
+
+function addThreeNumbers(num1, num2, num3) {   // a function will always return
+    return num1 + num2 + num3;
+}
+
+let result = addThreeNumbers(10, 20, 50);
+
+console.log("result: ",result);
