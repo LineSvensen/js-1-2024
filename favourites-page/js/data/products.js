f358b76361ccd26503b3b2be0e511d3f0c628ad8
MSI\lines <line.sven@live.no>
2024-01-16 16:25:09 +0100

    no message

---
+++ favourites-page/js/data/products.js
@@ -0,0 +1,19 @@
+export default [
+    {
+        id: 1,
+        name: "phone 4",
+        price: 200
+    },
+    {
+        id: 2,
+        name: "phone 6",
+        price: 300
+    },
+    {
+        id: 3,
+        name: "phone 7",
+        price: 600
+    }
+];
+
+// let name= "thomas"
