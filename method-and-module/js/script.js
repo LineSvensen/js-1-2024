a7f5b0f218941e80dd79c7d702f065a12f3ed883
MSI\lines <line.sven@live.no>
2024-02-09 12:05:52 +0100

    method + module

---
+++ method-and-module/js/script.js
@@ -0,0 +1,13 @@
+// THIS IN A SEPERATE JS FOLDER:
+// export function square(number) {
+//     return number * number;
+// }
+
+let someNumber = 9;
+
+import {square} from "mathfunction.js" // and then import in this js folder
+
+let squaredNumber = square(someNumber);
+
+
+// what we do here is MODULE :-)
